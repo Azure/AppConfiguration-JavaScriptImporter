9bd67cffea95b34bc0eeab713ec177f718e316ba
Maryanne Njeri <mgichohi@microsoft.com>
2024-06-28 13:54:27 +0300

    Add build and pack script

---
+++ scripts/build-and-pack.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Stop on error.
+set -e
+
+# Get the directory of the script.
+SCRIPT_DIR=$(dirname $(readlink -f $0))
+
+# Get the directory of the project.
+PROJECT_BASE_DIR=$(dirname $SCRIPT_DIR)
+
+echo $PROJECT_BASE_DIR
+# Change to the project directory.
+cd $PROJECT_BASE_DIR
+
+#Install dependencies, build, and test.
+echo "install pnpm"
+npm install -g pnpm
+
+echo "rush update"
+node common/scripts/install-run-rush.js update
+
+echo "rush install"
+node common/scripts/install-run-rush.js install
+
+echo "check format"
+node common/scripts/install-run-rush.js check-format --verbose
+
+echo "rush build"
+node common/scripts/install-run-rush.js build
+
+echo "test"
+node common/scripts/install-run-rush.js test --verbose
+
+IMPORTER_BASE_DIR=$PROJECT_BASE_DIR/libraries/azure-app-configuration-importer
+FILE_IMPORTER_SRC_BASE_DIR=$PROJECT_BASE_DIR/libraries/azure-app-configuration-importer-file-source
+
+# Create a tarball
+
+cd $IMPORTER_BASE_DIR
+
+echo "pack azure-app-configuration-importer"
+npm pack
+
+cd $FILE_IMPORTER_SRC_BASE_DIR
+
+echo "pack azure-app-configuration-importer-file-source"
+npm pack
